55c32cc4ed079f3158112e1c53cce1b143f01c88
StrikerBow <Joshuaian@live.com>
2021-06-12 13:52:48 -0300

    SEPTIMO EJERCICIO

---
+++ Ejercicio 7/anotherscript.js
@@ -0,0 +1,14 @@
+for(let i = 1; i<=500; i++){
+    document.write(i);
+    if(i % 4 == 0){
+        document.write(" | Es multiplo de 4")
+    }
+    else if(i % 9 == 0){
+
+        document.write(" | Es multiplo de 9")
+    }
+    document.write("<br>");
+    if(i % 5 == 0){
+        document.write("<hr>");
+    }
+}
